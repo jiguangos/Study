836f7b036c2c4fcc511bf6162923128c23a62873
tarena <tarena@tarenadeiMac-54.local>
2015-07-01 11:27:35 +0800

    test代码

---
+++ git测试/git测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git测试
+//
+//  Created by tarena on 15-7-1.
+//  Copyright (c) 2015年 Tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git测试/git测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git测试
+//
+//  Created by tarena on 15-7-1.
+//  Copyright (c) 2015年 Tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
